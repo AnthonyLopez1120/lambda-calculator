a8260b7ca55386bb874caee3571356d5b7be405c
Anthony Lopez <anthonybeconscious@gmail.com>
2019-11-05 20:15:37 -0600

    attempt stretch

--- src/App.js
+++ src/App.js
@@ -25,16 +25,22 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-          <div className = "display">
-            <Display value = {displayValue}/>
-          </div>
-          <div>
-            <Operators/>
-            <Numbers/>
-            <Specials/>
-          </div>
-          <div>
+         
 
+        
+            <div className = "display">
+              <Display value = {displayValue}/>
+            </div>
+            
+            <div className = "btnContain">
+            <div className = "leftButtons">
+              <Specials/>
+              <Numbers/>
+            </div>
+            <div className = "rightButtons">
+              <Operators/>
+            </div>
+          
           </div>
       </div>
     </div>
