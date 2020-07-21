83e11dd4bd4f24573014203c6fc036594ee4cde9
Anthony Lopez <anthonybeconscious@gmail.com>
2020-07-21 14:41:34 -0500

    fixing colors and styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Numbers from "./components/ButtonComponents/NumberButtons/Numbers"
 
 import Display from "./components/DisplayComponents/Display"
 import Logo from "./components/DisplayComponents/Logo"
+import NumberButton from "./components/ButtonComponents/NumberButtons/NumberButton";
 
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
@@ -13,7 +14,7 @@ import Logo from "./components/DisplayComponents/Logo"
 // Logo has already been provided for you. Do the same for the remaining components
 
 function App() {
-  const [displayValue] = useState(0)
+  const [displayValue, setDisplayVaule] = useState(0)
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
@@ -32,16 +33,15 @@ function App() {
               <Display value = {displayValue}/>
             </div>
             
-            <div className = "btnContain">
-            <div className = "leftButtons">
-              <Specials/>
-              <Numbers/>
-            </div>
-            <div className = "rightButtons">
+           
+           
+              <Specials />
+              <Numbers />
+           
+            
               <Operators/>
-            </div>
+           
           
-          </div>
       </div>
     </div>
   );
